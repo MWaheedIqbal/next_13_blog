497d4cf201da62f7f779a6eb786bd54067d7dd5d
WaheedIqbal <tesla.dxb@gmail.com>
2023-02-05 15:02:54 +0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,8 +10,11 @@ export default function Home() {
     <main className={styles.main}>
 
       <div className={styles.center}>
-        Hello World 
+      <h1>Home</h1>
       </div>
+      <Link href="/posts">
+        Posts 
+      </Link>
     </main>
-  )
+  );
 }
---
+++ app/posts/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+export default function Post() {
+    const router = useRouter();
+    return (
+    <main className={styles.main}>
+
+      <div className={styles.center}>
+        <h1>Post</h1> 
+      </div>
+      <Link href="/">
+        Back to Home 
+      </Link>
+      <button type="button" onClick={() => router.push('/')}>
+        Back to Home 
+      </button>
+    </main>
+  )
+}
