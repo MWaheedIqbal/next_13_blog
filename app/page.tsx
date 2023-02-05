05605eda966a4c89e4572f9163950b3937a221f5
WaheedIqbal <tesla.dxb@gmail.com>
2023-02-05 13:00:25 +0400

    Next_13

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+
+      <div className={styles.center}>
+        Hello World 
+      </div>
+    </main>
+  )
+}
